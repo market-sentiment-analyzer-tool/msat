92fc81cc70919a1c1fbd424b825c220790a7cb7d
Hugo Paré <hugopare12@gmail.com>
2024-02-08 10:50:39 -0500

    Added words to lexicon

--- scrappers/reddit-scrapper.py
+++ scrappers/reddit-scrapper.py
@@ -5,15 +5,16 @@ reddit = praw.Reddit(client_id='cbsZ48Da2i2eZ4AtTAQsjQ', client_secret='uk9kKwKp
 
 posts = []
 subreddit = reddit.subreddit('stocks')
-for post in subreddit.hot(limit=10):
-    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
-posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
-print(posts)
+# for post in subreddit.hot(limit=10):
+# # for post in subreddit.top(time_filter="day",limit=10):
+#     posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+# posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+# print(posts)
 
-# submission = reddit.submission(id="18888ww")
+submission = reddit.submission(id="1aldp6e")
 
-# comments = []
-# for top_level_comment in submission.comments:
-#     comments.append([top_level_comment.body])
-# comments = pd.DataFrame(comments,columns=['body'])
-# print(comments)   
+comments = []
+for top_level_comment in submission.comments:
+    comments.append([top_level_comment.body])
+comments = pd.DataFrame(comments,columns=['body'])
+print(comments)   
--- vaderSentiments/vaderSentiment/vaderSentiment.py
+++ vaderSentiments/vaderSentiment/vaderSentiment.py
@@ -36,7 +36,7 @@ NEGATE = \
      "dont", "hadnt", "hasnt", "havent", "isnt", "mightnt", "mustnt", "neither",
      "don't", "hadn't", "hasn't", "haven't", "isn't", "mightn't", "mustn't",
      "neednt", "needn't", "never", "none", "nope", "nor", "not", "nothing", "nowhere",
-     "oughtnt", "shant", "shouldnt", "uhuh", "wasnt", "werent",
+     "oughtnt", "shant", "shouldnt", "slash", "slashed", "slashes", "uhuh", "wasnt", "werent",
      "oughtn't", "shan't", "shouldn't", "uh-uh", "wasn't", "weren't",
      "without", "wont", "wouldnt", "won't", "wouldn't", "rarely", "seldom", "despite"]
 
@@ -44,15 +44,16 @@ NEGATE = \
 # http://en.wiktionary.org/wiki/Category:English_degree_adverbs
 
 BOOSTER_DICT = \
-    {"absolutely": B_INCR, "amazingly": B_INCR, "awfully": B_INCR,
+    {"absolutely": B_INCR, "amazingly": B_INCR, "awfully": B_INCR, "beats": B_INCR,
      "completely": B_INCR, "considerable": B_INCR, "considerably": B_INCR,
-     "decidedly": B_INCR, "deeply": B_INCR, "effing": B_INCR, "enormous": B_INCR, "enormously": B_INCR,
+     "decidedly": B_INCR, "deeply": B_INCR, "definitely": B_INCR, "double-digit": B_INCR, "effing": B_INCR, "enormous": B_INCR,
+     "enormously": B_INCR,
      "entirely": B_INCR, "especially": B_INCR, "exceptional": B_INCR, "exceptionally": B_INCR,
      "extreme": B_INCR, "extremely": B_INCR,
      "fabulously": B_INCR, "flipping": B_INCR, "flippin": B_INCR, "frackin": B_INCR, "fracking": B_INCR,
      "fricking": B_INCR, "frickin": B_INCR, "frigging": B_INCR, "friggin": B_INCR, "fully": B_INCR,
      "fuckin": B_INCR, "fucking": B_INCR, "fuggin": B_INCR, "fugging": B_INCR,
-     "greatly": B_INCR, "hella": B_INCR, "highly": B_INCR, "hugely": B_INCR,
+     "greatly": B_INCR, "hella": B_INCR, "highly": B_INCR, "hugely": B_INCR, "hikes": B_INCR,
      "incredible": B_INCR, "incredibly": B_INCR, "intensely": B_INCR,
      "major": B_INCR, "majorly": B_INCR, "more": B_INCR, "most": B_INCR, "particularly": B_INCR,
      "purely": B_INCR, "quite": B_INCR, "really": B_INCR, "remarkably": B_INCR,
@@ -64,7 +65,7 @@ BOOSTER_DICT = \
      "kind of": B_DECR, "kinda": B_DECR, "kindof": B_DECR, "kind-of": B_DECR,
      "less": B_DECR, "little": B_DECR, "marginal": B_DECR, "marginally": B_DECR,
      "occasional": B_DECR, "occasionally": B_DECR, "partly": B_DECR,
-     "scarce": B_DECR, "scarcely": B_DECR, "slight": B_DECR, "slightly": B_DECR, "somewhat": B_DECR,
+     "scarce": B_DECR, "scarcely": B_DECR, "single-digit": B_DECR, "slight": B_DECR, "slightly": B_DECR, "somewhat": B_DECR,
      "sort of": B_DECR, "sorta": B_DECR, "sortof": B_DECR, "sort-of": B_DECR}
 
 # check for sentiment laden idioms that do not contain lexicon words (future work, not yet implemented)
