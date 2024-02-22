3274c05832148a181f6c26abdfaf70b66fa6e26c
Jayden Bergevin <Jberg114@uottawa.ca>
2024-02-22 09:58:41 -0500

    Added comments to files

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
+-- Crée une base de données appelée MarketSentiment si elle n'existe pas déjà
 CREATE DATABASE IF NOT EXISTS MarketSentiment;
 
-
+-- Sélectionne la base de données MarketSentiment pour travailler dessus
 USE MarketSentiment;
 
-
-
+-- Crée une table appelée NVDA si elle n'existe pas déjà
 CREATE TABLE IF NOT EXISTS NVDA (
     subreddit VARCHAR(255),
     post_id INT,
@@ -13,4 +13,4 @@ CREATE TABLE IF NOT EXISTS NVDA (
     score INT,
     sentiment FLOAT,
     p_description TEXT
-)
+);
