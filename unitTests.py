5dda26546ca61df9dc622e53c0d47bcc9a9f58a2
Hugo Paré <hugopare12@gmail.com>
2024-03-07 16:51:25 -0500

    Update unitTests.py

--- unitTests.py
+++ unitTests.py
@@ -16,7 +16,7 @@ class TestSentimentAnalyzer(unittest.TestCase):
 
     # Testing positive comments
 
-    def test_positive_comment_1(self):
+    def test_positive_comment_1001(self):
         # Positive comment, compound expected > 0
         text = "A buy back is always bullish. Think of it basically like a dividend lol. It reduces the float which raises the avg price per share."
         analyzer = SentimentIntensityAnalyzer()
@@ -24,7 +24,7 @@ class TestSentimentAnalyzer(unittest.TestCase):
         compound_value = vs.get('compound')
         self.assertTrue(compound_value > 0)
 
-    def test_positive_comment_2(self):
+    def test_positive_comment_1002(self):
         # Positive comment, compound expected > 0
         text = "This is great news for all semi stocks and tech in general. Up we go."
         analyzer = SentimentIntensityAnalyzer()
@@ -32,7 +32,7 @@ class TestSentimentAnalyzer(unittest.TestCase):
         compound_value = vs.get('compound')
         self.assertTrue(compound_value > 0)
 
-    def test_positive_comment_3(self):
+    def test_positive_comment_1003(self):
         # Positive comment, compound expected > 0
         text = "Disney beats earnings estimates, hikes guidance as it slashes streaming losses"
         analyzer = SentimentIntensityAnalyzer()
@@ -40,7 +40,7 @@ class TestSentimentAnalyzer(unittest.TestCase):
         compound_value = vs.get('compound')
         self.assertTrue(compound_value > 0)
 
-    def test_positive_comment_4(self):
+    def test_positive_comment_1004(self):
         # Positive comment, compound expected > 0
         text = "Uber beats estimates as revenue and bookings see double-digit growth"
         analyzer = SentimentIntensityAnalyzer()
@@ -50,7 +50,7 @@ class TestSentimentAnalyzer(unittest.TestCase):
 
     # Testing negative comments
         
-    def test_negative_comment_1(self):
+    def test_negative_comment_1001(self):
         # Negative comment, compound expected < 0
         text = "RSI is like 80. it's overbought. I wouldn't buy TSM or NVDA right now. If I wanted more exposure to them I might buy SMH."
         analyzer = SentimentIntensityAnalyzer()
@@ -60,7 +60,7 @@ class TestSentimentAnalyzer(unittest.TestCase):
 
     # Testing mixed sentiment comments
         
-    def test_mixed_comment_1(self):
+    def test_mixed_comment_1001(self):
         # Mixed comment, compound expected > 0
         text = "NVDA is so overvalued right now, but on the other hand, it has so much potential for growth."
         analyzer = SentimentIntensityAnalyzer()
@@ -68,7 +68,7 @@ class TestSentimentAnalyzer(unittest.TestCase):
         compound_value = vs.get('compound')
         self.assertTrue(compound_value > 0)
 
-    def test_mixed_comment_2(self):
+    def test_mixed_comment_1002(self):
         # Mixed comment, compound expected < 0
         text = "NVDA has so much potential, but on the other hand, it is so overvalued right now."
         analyzer = SentimentIntensityAnalyzer()
@@ -84,14 +84,14 @@ class TestRedditApi(unittest.TestCase):
     # Time filter (hour, day, week, year)
     # stock_filter = [""] # no filter
 
-    def test_good_request_1(self):
+    def test_good_request_1001(self):
         # Get posts table
         time_filter = "hour"
         stock_filter = ["aapl","apple"]
         posts = getPostsTable(time_filter,stock_filter)
         self.assertGreaterEqual(len(posts),0)
 
-    def test_good_request_2(self):
+    def test_good_request_1002(self):
         # Get posts table
         time_filter = "day"
         stock_filter = ["aapl","apple"]
@@ -116,19 +116,39 @@ class TestRedditApi(unittest.TestCase):
     # Time filter (hour, day, week, year)
     # stock_filter = [""] # no filter
         
-    def test_bad_request_1(self):
+    def test_bad_request_1001(self):
         # Request without stock_filter
         time_filter = "hour"
         self.assertRaises(TypeError,getPostsTable,time_filter)
     
-    def test_bad_request_2(self):
+    def test_bad_request_1002(self):
         # Request without time_filter
         stock_filter = ["aapl","apple"]
         self.assertRaises(TypeError,getPostsTable,stock_filter)
 
-    def test_bad_request_2(self):
+    def test_bad_request_1003(self):
         # Request without any arguments
         self.assertRaises(TypeError,getPostsTable)
+
+    def test_bad_request_1004(self):
+        # Request with time_filter argument not accepted
+        time_filter = "none"
+        stock_filter = ["aapl","apple"]
+        self.assertRaises(ValueError,getPostsTable,time_filter,stock_filter)
+
+    def test_bad_request_2001(self):
+        # Request without stock_filter
+        time_filter = "hour"
+        self.assertRaises(TypeError,getCommentsTable,time_filter)
+    
+    def test_bad_request_2002(self):
+        # Request without time_filter
+        stock_filter = ["aapl","apple"]
+        self.assertRaises(TypeError,getCommentsTable,stock_filter)
+
+    def test_bad_request_2003(self):
+        # Request without any arguments
+        self.assertRaises(TypeError,getCommentsTable)
 
 
 if __name__ == '__main__': 
