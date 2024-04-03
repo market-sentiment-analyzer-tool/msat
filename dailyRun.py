e439fb41a2a1b0242b27dd4a2dcf2c8113609cb0
Jayden Bergevin <Jberg114@uottawa.ca>
2024-04-03 09:04:11 -0400

    Modified daily run

--- dailyRun.py
+++ dailyRun.py
@@ -5,40 +5,47 @@ from sentimentAnalyzer import  append_comments, append_posts, close_db_connectio
 
 
 #Hourly posts and comments
-time_filter = "hour"
-stock_filter = ['nvda', 'nvidia']
-hourly_comments = getCommentsTable(time_filter,stock_filter)
-hourly_posts = getPostsTable(time_filter,stock_filter)
-append_comments(hourly_comments)
-append_posts(hourly_posts)
+def daily():
+    time_filter = "hour"
+    stock_filter = ['nvda', 'nvidia']
+    hourly_comments = getCommentsTable(time_filter,stock_filter)
+    hourly_posts = getPostsTable(time_filter,stock_filter)
+    append_comments(hourly_comments)
+    append_posts(hourly_posts)
+    print("Hourly done")
 
 #Daily posts and comments
-time_filter = "day"
-stock_filter = ['nvda', 'nvidia']
-daily_comments = getCommentsTable(time_filter,stock_filter)
-daily_posts = getPostsTable(time_filter,stock_filter)
-append_comments(daily_comments)
-append_posts(daily_posts)
+def dayly():
+    time_filter = "day"
+    stock_filter = ['nvda', 'nvidia']
+    daily_comments = getCommentsTable(time_filter,stock_filter)
+    daily_posts = getPostsTable(time_filter,stock_filter)
+    append_comments(daily_comments)
+    append_posts(daily_posts)
+    print("Daily done")
 
 
 #Weekly post and comments
-time_filter = "week"
-stock_filter = ['nvda', 'nvidia']
-weekly_comments = getCommentsTable(time_filter,stock_filter)
-weekly_posts = getPostsTable(time_filter,stock_filter)
-append_comments(weekly_comments)
-append_posts(weekly_posts)
+def weekly():
+    time_filter = "week"
+    stock_filter = ['nvda', 'nvidia']
+    weekly_comments = getCommentsTable(time_filter,stock_filter)
+    weekly_posts = getPostsTable(time_filter,stock_filter)
+    append_comments(weekly_comments)
+    append_posts(weekly_posts)
+    print("Weekly done")
 
 #Yearly post and comments
-time_filter = "year"
-stock_filter = ['nvda', 'nvidia']
-weekly_comments = getCommentsTable(time_filter,stock_filter)
-weekly_posts = getPostsTable(time_filter,stock_filter)
-append_comments(weekly_comments)
-append_posts(weekly_posts)
+def yearly():
+    time_filter = "year"
+    stock_filter = ['nvda', 'nvidia']
+    weekly_comments = getCommentsTable(time_filter,stock_filter)
+    weekly_posts = getPostsTable(time_filter,stock_filter)
+    append_comments(weekly_comments)
+    append_posts(weekly_posts)
+    ("Yearly done")
 
-
-close_db_connection()
+yearly()
 
 
 
