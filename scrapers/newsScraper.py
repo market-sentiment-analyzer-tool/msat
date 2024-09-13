8d68f48990d8a9d82d67fd1e44c72a614712f01d
Hugo Pare <hpare@hugo>
2024-09-13 10:53:35 -0400

    News scraper working

--- scrapers/newsScraper.py
+++ scrapers/newsScraper.py
@@ -3,57 +3,161 @@ import os
 # Add parent directory to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from creds import credentials
+from vaderSentiments.vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
-#############
-# News API
-#############
+# Examples of parameters
+stock = 'NVDA,Nvidia'
+# stock = 'AAPL,Apple'
 
-# url = ('https://newsapi.org/v2/everything?'         # same url for every stock
-#        'q=Nvidia&'                                  # change depending on stock
-#        'from=2024-09-10&'                           # change to previous day, will run daily
-#        'sortBy=popularity&'                         # keep it as is
-#        'apiKey=credentials["apikeys"]["newsapi"]')   # add apiKey to secrets file
+def getPageContent(url):
+    try:
+        # Send an HTTP request to the URL
+        response = requests.get(url)
+        # Parse the HTML content
+        soup = BeautifulSoup(response.content, "html.parser")
+        # Find all <p> tags and extract their text
+        p_tags = soup.find_all('p')
+        p_texts = [p.get_text() for p in p_tags]
+        # Join all the text from the <p> tags and print it
+        content = "\n".join(p_texts)
+        return content
+    except:
+        raise Exception(f"Failed to retrieve the page.")
+    
+def calculateSentiment(title,content):
+    analyzer = SentimentIntensityAnalyzer()
+    # Get title sentiment
+    vs = analyzer.polarity_scores(title)
+    title_sentiment = vs.get('compound')
+    # Get content sentiment
+    vs = analyzer.polarity_scores(content)
+    content_sentiment = vs.get('compound')
+    # Sentiment formula
+    sentiment = title_sentiment * 0.75 + content_sentiment * 0.25
+    return sentiment
 
-#############
-# News Data
-#############
+def fetchNewsAPI(stock):
+    # Keep track of URLs
+    urls = []
+    # Keep track of data added to DB
+    data = []
 
-# url = ('https://newsdata.io/api/1/latest?'
-#        'apikey=credentials["apikeys"]["newsdata"]&'
-#        'country=us,ca&'
-#        'q=Nvidia,NVDA&'
-#        'category=business&'
-#        'language=en&'
-#        'removeduplicate=1')
-# response = requests.get(url)
+    # Yesterday's date, since News API is delayed by 24 hours
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    yesterday = (datetime.strptime(current_date, "%Y-%m-%d") - timedelta(days=1)).strftime("%Y-%m-%d")
 
-# print(response.json())
+    # Call News API
+    news_api_url = ('https://newsapi.org/v2/everything?'            # same url for every stock
+                    f'q={stock}&'                                   # change depending on stock
+                    f'from={yesterday}&'                            # change to previous day, will run daily
+                    'sortBy=popularity&'                         
+                    f'apiKey={credentials["apikeys"]["newsapi"]}')  # apiKey in secrets file
+    json = requests.get(news_api_url)
+    response = json.json()
 
+    # Number of articles retrieved
+    number_articles = response['totalResults']
 
+    for article in range(number_articles):
+        title = response['articles'][article]['title']
+        url = response['articles'][article]['url']
+        content = response['articles'][article]['content']
+        full_content = getPageContent(url)
+        author = response['articles'][article]['author']
 
-# Getting page content
-# Specify the URL
-# url = "https://finance.yahoo.com/news/live/stock-market-today-nasdaq-nvidia-lead-rally-as-inflation-reading-cements-bets-on-small-fed-rate-cut-111124850.html"
-# url = "https://www.benzinga.com/markets/cryptocurrency/24/09/40807307/bitcoin-ethereum-dogecoin-give-up-gains-as-trump-harris-skip-mention-of-crypto-in-presiden"
-# url = "https://seekingalpha.com/article/4720322-harris-associates-us-large-value-strategy-q2-2024-commentary?source=feed_all_articles"
+        # date of publication
+        date_string = response['articles'][article]['publishedAt']
+        date_object = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
+        date = date_object.strftime("%Y-%m-%d")
 
-# # Send an HTTP request to the URL
-# response = requests.get(url)
+        # check if url is already part of DB
 
-# # If the request was successful, proceed to parse the HTML
-# if response.status_code == 200:
-#     # Parse the HTML content
-#     soup = BeautifulSoup(response.content, "html.parser")
+        # check if url is already fetched
+        if url not in urls:
+            # proceed to add it to DB
+            urls.append(url)
+            # analyze sentiment of article
+            sentiment = calculateSentiment(title=title,content=full_content)
+            # create json for article
+            article_data = {
+                "author": author,
+                "content": content,
+                "date": date,
+                "sentiment": sentiment,
+                "title": title,
+                "url": url
+            }
+            data.append(article_data)
+    # Return data
+    return urls, data
 
-#     # Find all <p> tags and extract their text
-#     p_tags = soup.find_all('p')
-#     p_texts = [p.get_text() for p in p_tags]
+def fetchNewsData(stock,urls,data):
+    # Keep track of urls and data from previous API call
+    urls = urls
+    data = data
+    # Call News Data
+    news_data_url = ('https://newsdata.io/api/1/latest?'
+                    f'apikey={credentials["apikeys"]["newsdata"]}&'
+                    'country=us,ca&'
+                    f'q={stock}&'
+                    'category=business&'
+                    'language=en&'
+                    'removeduplicate=1&'
+                    'size=10')
+    json = requests.get(news_data_url)
+    response = json.json()
 
-#     # Join all the text from the <p> tags and print it
-#     all_p_text = "\n".join(p_texts)
-#     print(all_p_text)
-# else:
-#     print(f"Failed to retrieve the page. Status code: {response.status_code}")
+    # Number of articles retrieved
+    number_articles = len(response['results'])
+
+    for article in range(number_articles):
+        title = response['results'][article]['title']
+
+        # content
+        url = response['results'][article]['link']
+        full_content = getPageContent(url)
+        content = full_content[:200] + "..."
+
+        # author
+        creator = response['results'][article]['creator']
+        author = creator[0] if creator else None
+
+        # date of publication
+        date_string = response['results'][article]['pubDate']
+        date_object = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+        date = date_object.strftime("%Y-%m-%d")
+
+        # check if url is already part of DB
+
+        # check if url is already fetched
+        if url not in urls:
+            # proceed to add it to DB
+            urls.append(url)
+            # analyze sentiment of article
+            sentiment = calculateSentiment(title=title,content=full_content)
+            # create json for article
+            article_data = {
+                "author": author,
+                "content": content,
+                "date": date,
+                "sentiment": sentiment,
+                "title": title,
+                "url": url
+            }
+            data.append(article_data)
+    return data
+
+def fetchNewsAPIs(stock):
+    # Fetch News API
+    urls, data = fetchNewsAPI(stock=stock)
+    # Fetch News Data
+    data = fetchNewsData(stock=stock,urls=urls,data=data)
+    # Return data formatted in json
+    return data
+
+data = fetchNewsAPIs(stock=stock)
+print(data)
