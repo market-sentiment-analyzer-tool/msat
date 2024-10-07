f8886d019328691b1524a2be4c29155056f1c97d
Hugo Paré <hpare040@uottawa.ca>
2024-10-07 16:13:01 -0400

    Update dumpData.sh

--- db/dumpData.sh
+++ db/dumpData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to the stockInfo.json file
-stock_info_file="./stockInfo.json"
+stock_info_file="stockInfo.json"
 
 # Check if the file exists
 if [ ! -f "$stock_info_file" ]; then
