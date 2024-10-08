e27a6ba47577b1a77aee683aee601027d58bc6ca
Hugo Pare <hpare@hugo>
2024-10-07 21:10:15 -0400

    Fixing issue with db init

--- db/initializeDatabase.sh
+++ db/initializeDatabase.sh
@@ -9,7 +9,7 @@ DATA_DIR="./db/data/news/"
 # Loop over each .sql file in the data directory and insert it into the database
 for sql_file in "$DATA_DIR"*.sql; do
     echo "Importing data from $sql_file..."
-    docker exec mysql-db mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < $sql_file
+    docker exec -i mysql-db mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < $sql_file
     if [ $? -eq 0 ]; then
         echo "Data from $sql_file imported successfully."
     else
