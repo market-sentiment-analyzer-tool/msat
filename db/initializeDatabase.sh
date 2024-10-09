0e5208d46f22b09a8d1aa40e722e33154ea8b5b4
Hugo Pare <hpare@hugo>
2024-10-09 14:18:29 -0400

    Fixing bug where DB init was not working

--- db/initializeDatabase.sh
+++ db/initializeDatabase.sh
@@ -10,7 +10,7 @@ NEWS_DATA_DIR="./db/data/news/"
 # Loop over each .sql file in the data/news directory and insert it into the database
 for sql_file in "$NEWS_DATA_DIR"*.sql; do
     echo "Importing data from $sql_file..."
-    docker exec -i mysql-db mysql -u root -p$MYSQL_ROOT_PASSWORD --no-beep $MYSQL_DATABASE < $sql_file 2>/dev/null
+    docker exec -i mysql-db mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < $sql_file 2>/dev/null
     if [ $? -eq 0 ]; then
         echo "Data from $sql_file imported successfully."
     else
@@ -20,7 +20,7 @@ done
 # Loop over each .sql file in the data/reddit directory and insert it into the database
 for sql_file in "$REDDIT_DATA_DIR"*.sql; do
     echo "Importing data from $sql_file..."
-    docker exec -i mysql-db mysql -u root -p$MYSQL_ROOT_PASSWORD --no-beep $MYSQL_DATABASE < $sql_file 2>/dev/null
+    docker exec -i mysql-db mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < $sql_file 2>/dev/null
     if [ $? -eq 0 ]; then
         echo "Data from $sql_file imported successfully."
     else
