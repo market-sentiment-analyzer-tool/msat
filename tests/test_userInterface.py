2a384acec2d2c4a4e45f682ae9c74fcedbd03a20
Hugo Pare <hpare040@uottawa.ca>
2024-11-11 19:59:08 -0500

    Added test templatae for UI

---
+++ tests/test_userInterface.py
@@ -0,0 +1,10 @@
+import unittest
+
+class UserInterfaceTests(unittest.TestCase):
+
+    # Will need to add tests once UI is fully implemented
+    def test(self):
+        assert(True)
+
+if __name__ == '__main__': 
+    unittest.main() 
